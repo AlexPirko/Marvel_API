5c6b2e880d3768df27bf52060ffe4643521d4811
AlexPirko <youzhny.foto@gmail.com>
2023-02-12 17:38:29 +0200

    create react task

--- marvel_starter/src/services/MarvelService.js
+++ marvel_starter/src/services/MarvelService.js
@@ -1,19 +1,20 @@
 class MarvelService {
     _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     _apiKey = 'apikey=e467a02465cf798746d6cf7e73f04b32';
+    _baseOffset = 210;
 
     getResource = async (url) => {
         let res = await fetch(url);
 
         if (!res.ok) {
-            throw new Error(`Could not fetch $(url), status: ${res.status}`);
+            throw new Error(`Could not fetch ${url}, status: ${res.status}`);
         }
 
-        return await res.json()
+        return await res.json();
     }
 
-    getAllCharactes = async () => {
-        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllCharacters = async (offset = this._baseOffset) => {
+        const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=${offset}&${this._apiKey}`);
         return res.data.results.map(this._transformCharacter);
     }
 
@@ -24,11 +25,13 @@ class MarvelService {
 
     _transformCharacter = (char) => {
         return {
+            id: char.id,
             name: char.name,
-            description: char.description,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
-            wiki: char.urls[1].url
+            wiki: char.urls[1].url,
+            comics: char.comics.items
         }
     }
 }
